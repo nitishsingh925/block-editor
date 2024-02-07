958d96229b2901eae48fcbfa146b48b8895764ab
Nitish Singh <nitishsingh925@gmail.com>
2024-02-07 20:14:18 +0530

    Add drag and drop functionality using react-beautiful-dnd

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { PlusOutlined } from "@ant-design/icons";
 import TextBlock from "./TextBlock";
 import PictureBlock from "./PictureBlock";
 import "./App.css";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 const App = () => {
   const [blocks, setBlocks] = useState([]);
@@ -27,37 +28,71 @@ const App = () => {
     );
   };
 
-  return (
-    <div className="container">
-      <Button
-        type="primary"
-        icon={<PlusOutlined />}
-        className="addButton"
-        onClick={() => handleAddBlock("text")}
-      >
-        Add Text Block
-      </Button>
-      <Button
-        type="primary"
-        icon={<PlusOutlined />}
-        className="addButton"
-        onClick={() => handleAddBlock("picture")}
-      >
-        Add Picture Block
-      </Button>
+  const handleDragEnd = (result) => {
+    if (!result.destination) return;
 
-      {blocks.map((block) => (
-        <div key={block.id} className="blockContainer">
-          {block.type === "text" && (
-            <TextBlock
-              content={block.content}
-              onChange={(value) => handleBlockContentChange(block.id, value)}
-            />
+    const updatedBlocks = Array.from(blocks);
+    const [movedBlock] = updatedBlocks.splice(result.source.index, 1);
+    updatedBlocks.splice(result.destination.index, 0, movedBlock);
+
+    setBlocks(updatedBlocks);
+  };
+
+  return (
+    <DragDropContext onDragEnd={handleDragEnd}>
+      <div className="container">
+        <Button
+          type="primary"
+          icon={<PlusOutlined />}
+          className="addButton"
+          onClick={() => handleAddBlock("text")}
+        >
+          Add Text Block
+        </Button>
+        <Button
+          type="primary"
+          icon={<PlusOutlined />}
+          className="addButton"
+          onClick={() => handleAddBlock("picture")}
+        >
+          Add Picture Block
+        </Button>
+
+        <Droppable droppableId="blocks">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {blocks.map((block, index) => (
+                <Draggable
+                  key={block.id}
+                  draggableId={block.id.toString()}
+                  index={index}
+                >
+                  {(provided) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      className="blockContainer"
+                    >
+                      {block.type === "text" && (
+                        <TextBlock
+                          content={block.content}
+                          onChange={(value) =>
+                            handleBlockContentChange(block.id, value)
+                          }
+                        />
+                      )}
+                      {block.type === "picture" && <PictureBlock />}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
           )}
-          {block.type === "picture" && <PictureBlock />}
-        </div>
-      ))}
-    </div>
+        </Droppable>
+      </div>
+    </DragDropContext>
   );
 };
 
