622d713ec96de33c28119f42940c73f4350fd7fc
Nitish Singh <nitishsingh925@gmail.com>
2024-02-08 08:02:20 +0530

    Refactor PictureBlock component to include image preview and modal

--- src/PictureBlock.js
+++ src/PictureBlock.js
@@ -1,22 +1,60 @@
-// PictureBlock.js
-
-import React from "react";
-import { Upload, Button, message } from "antd";
+import React, { useState } from "react";
+import { Upload, Button, message, Modal } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
 
-const PictureBlock = () => (
-  <Upload
-    beforeUpload={() => false}
-    onChange={(info) => {
-      if (info.file.status === "done") {
-        message.success(`${info.file.name} uploaded successfully`);
-      }
-    }}
-  >
-    <Button icon={<PlusOutlined />} className="uploadButton">
-      Upload Picture
-    </Button>
-  </Upload>
-);
+const PictureBlock = () => {
+  const [preview, setPreview] = useState({
+    open: false,
+    image: "",
+    title: "",
+  });
+  const [fileList, setFileList] = useState([]);
+
+  const getBase64 = (file) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = (error) => reject(error);
+    });
+
+  return (
+    <>
+      <Upload
+        beforeUpload={() => false}
+        listType="picture-card"
+        fileList={fileList}
+        onPreview={async (file) => {
+          if (!file.url && !file.preview) {
+            file.preview = await getBase64(file.originFileObj);
+          }
+
+          setPreview({
+            open: true,
+            image: file.url || file.preview,
+            title:
+              file.name || file.url.substring(file.url.lastIndexOf("/") + 1),
+          });
+        }}
+        onChange={({ fileList: newFileList }) => setFileList(newFileList)}
+      >
+        {fileList.length >= 8 ? null : (
+          <div>
+            <PlusOutlined />
+            <div className="ant-upload-text">Upload</div>
+          </div>
+        )}
+      </Upload>
+      <Modal
+        visible={preview.open}
+        title={preview.title}
+        footer={null}
+        onCancel={() => setPreview({ open: false, image: "", title: "" })}
+      >
+        <img alt="example" style={{ width: "100%" }} src={preview.image} />
+      </Modal>
+    </>
+  );
+};
 
 export default PictureBlock;
